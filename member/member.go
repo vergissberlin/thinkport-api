55c340911a9be51af9ae2c15d105a6f01f677b98
André Lademann <alademann@thinkport.digital>
2023-06-26 21:24:57 +0200

    Add members dummy

---
+++ member/member.go
@@ -0,0 +1,16 @@
+package member
+
+import (
+	"context"
+)
+
+type Response struct {
+	Members string `json:"members"`
+}
+
+// Returns all members
+// encore:api public path=/members/ method=GET
+func World(ctx context.Context, name string) (*Response, error) {
+	msg := &Response{Members: "All Members "}
+	return msg, nil
+}
